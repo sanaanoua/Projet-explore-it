86f7a83ac9d73a0a5dec17c78df829fc93043ef6
Greg155 <gregakhun@gmail.com>
2020-10-29 08:41:42 +0100

    Update of the map

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import MainMap from './Components/MainMap'
+
+
+function App() {
+  return (
+    <div className="App">
+      <MainMap />
+    </div>
+
+  )
+  
+}
+
+export default App;
